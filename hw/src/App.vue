2b8cbf326338c4ec31d46dd14dc880fc333f099c
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-02-17 12:19:09 +0300

    added getting values of answers

--- hw/src/App.vue
+++ hw/src/App.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
         <div v-for="q in questions">
-            <A :answers="q.answers" :title="q.title" :type="q.type" :qw_name="q.id"></A>
+            <A v-if="q.id === qn"
+                    :answers="q.answers"
+                    :title="q.title"
+                    :type="q.type"
+                    :qw_name="q.id"
+                    :qid="q.id">
+                </A>
         </div>
+        <button v-on:click="qn != 1 ? qn = qn - 1 : qn = qn">Назад</button>
+        <button v-on:click="qn != 10 ? qn = qn + 1 : qn = qn">Вперёд</button>
     </div>
 </template>
 
@@ -12,8 +20,13 @@ import json from './recources/data.json'
 export default {
     data(){
         return{
-            questions: json
+            questions: json,
+            qn : 3,
+            gAnswers: { 1 : [], 2 : [], 3 : [], 4 : [], 5 : [], 6 : [], 7 : [], 8 : [], 9 : [], 10 : []}
         }
+    },
+    methods: {
+        //receiveData(s, n)
     },
     components: {
 		A
--- hw/src/components/Question.vue
+++ hw/src/components/Question.vue
@@ -1,14 +1,28 @@
 <template>
     <div>
         <h2>{{ title }}</h2>
-        <p v-for="a in answers"><input :type="type" value="a" :name="qw_name">{{ a.ans }}</p>
+        <p v-if="type === 'checkbox'" v-for="a in answers"><input :type="type" :value="a.ans" :name="qw_name" v-model="checkedAnswers" v-on:change="onChange">{{ a.ans }}</p>
+        <p v-if="type === 'radio'" v-for="a in answers"><input :type="type" :value="a.ans" :name="qw_name" v-model="picked" v-on:change="onChange">{{ a.ans }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['title', 'type', 'answers', 'qw_name']
+    props: ['title', 'type', 'answers', 'qw_name', 'qid'],
+    data(){
+        return{
+            checkedAnswers: [],
+            picked: "",
+        }
+    },
+    methods: {
+        onChange(){
+            this.type === "checkbox" ? console.log(this.checkedAnswers) : console.log(this.picked)
+            //this.$emit('senddata', e, this.qid);
+        }
+    }
 }
+//v-on:change="onChange($event.target.value)"
 </script>
 
 <style>
