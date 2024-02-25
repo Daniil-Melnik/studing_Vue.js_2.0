2cff4be37b2623b4941491c5c43529afd2ca705b
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-02-25 17:23:35 +0300

    added product list

--- hw7/src/components/ProductsList.vue
+++ hw7/src/components/ProductsList.vue
@@ -1,30 +1,45 @@
 <template>
 	<div>
 		<h1>Каталог</h1>
-		<button @click="goBack">Назад</button>
 		<div class="row">
-			<div class="col col-sm-4" 
-				v-for="product, i in products" :key="i">
-				<a><h3>{{ product.name }}</h3></a>
-				<div>{{ product.price }}</div>
-				<button v-if="isInCart(i, this.$store.state.cart)"
-						class="btn btn-primary"
-						@click="addToCart(product, this.$store.state.cart)"
-						>
-					В корзину
-				</button>
-				<button v-else
-						class="btn btn-warning"
-						@click="removeFromCart(i, this.$store.state.cart)"
-				>
-					Убрать
-				</button>
-				<router-link :to="'/products/' + product.id ">
-					Подробнее
-				</router-link>
+			<div class="table-ab">
+				<table class="main-table-cat">
+					<tr>
+						<th>№</th>
+						<th></th>
+						<th>Наименование</th>
+						<th>Цена</th>
+						<th></th>
+					</tr>
+					<tr v-for="p, i in products" :key="i">
+						<td class="td-num">{{ i + 1 }}</td>
+						<td class="td-img-cat"><img :src="require(`@/assets/${p.img}`)"/></td>
+						<td class="td-name">{{ p.name }}</td>
+						<td class="td-price">{{ p.price }}</td>
+						<td class="td-info">
+							<router-link :to="'/products/' + p.id ">
+								Подробнее
+							</router-link>
+						</td>
+						<td>
+							<button v-if="isInCart(i, this.$store.state.cart)"
+									class="add-btn"
+									@click="addToCart(p, this.$store.state.cart)"
+									>
+								В корзину
+							</button>
+							<button  v-else
+									class="remove-btn"
+									@click="removeFromCart(p.id, this.$store.state.cart)">
+								Убрать
+							</button>
+						</td>
+					</tr>
+				</table>
+				</div>
 			</div>
+			<button class="before-btn" @click="goBack">Назад</button>
 		</div>
-	</div>
 </template>
 
 <script>
@@ -71,4 +86,76 @@
 	.row{
 		padding-left: 15px;
 	}
+
+	.main-table-cat{
+		width: 900px;
+	}
+	
+	.main-table-cat tr {
+		height: 35px;
+		font-size: 1.3em;
+		color: rgb(0, 17, 61);
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+	}
+	.main-table-cat td {
+		border-radius: 8px;
+	}
+	
+	.main-table-cat th {
+		border-radius: 8px;
+		background-color: rgb(122, 189, 255);
+	}
+
+	.td-img-cat img {
+		width: 35px;
+		height: 35px;
+	}
+	
+	.td-img-cat {
+		width: 35px;
+		background-color: rgb(243, 249, 255);
+		text-align: center;
+	}
+
+	.td-info {
+		width: 90px;
+		background-color: rgb(243, 249, 255);
+		text-align: center;
+		color: rgb(0, 17, 61);
+		text-decoration: none;
+	}
+
+	.add-btn{
+			height: 23px;
+			width: 80px;
+			margin-left: 8px;
+			border: 0;
+			background-color: rgb(175, 255, 129);
+			border-radius: 10px;
+			font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+			color: rgb(0, 17, 61);
+			transition: 0.6s;
+	}
+	.add-btn:hover {
+		background-color: rgb(0, 103, 0);
+		color: rgb(203, 229, 255);
+		transform: scale(1.3);
+	}
+
+	.remove-btn{
+		height: 23px;
+		width: 80px;
+		margin-left: 8px;
+		border: 0;
+		background-color: rgb(255, 129, 129);
+		border-radius: 10px;
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+		color: rgb(0, 17, 61);
+		transition: 0.6s;
+}
+.remove-btn:hover {
+	background-color: rgb(199, 0, 0);
+	color: rgb(203, 229, 255);
+	transform: scale(1.3);
+}
 </style>
